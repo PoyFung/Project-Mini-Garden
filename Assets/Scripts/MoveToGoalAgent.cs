e759f49aac0060c4417e9ec0e8142b3e2105d051
Brandon Tan <btan62747@gmail.com>
2024-03-30 21:39:14 -0400

    Update

--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -20,8 +20,8 @@ public class MoveToGoalAgent : Agent
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
+        float moveX = actions.ContinuousActions[0]*2;
+        float moveZ = actions.ContinuousActions[1] * 2;
 
         float moveSpeed = 1f;
         transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
@@ -30,8 +30,8 @@ public class MoveToGoalAgent : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continuousActions= actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[1] = Input.GetAxisRaw("Vertical");
+        continuousActions[0] = Input.GetAxisRaw("Horizontal")*2;
+        continuousActions[1] = Input.GetAxisRaw("Vertical")*2;
     }
 
     private void OnTriggerEnter(Collider other)
